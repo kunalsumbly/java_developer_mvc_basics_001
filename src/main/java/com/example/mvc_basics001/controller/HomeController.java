4bb78a69dad5cc7e830ef275ec078954ab16f625
kunalsumbly <kunal.sumbly@gmail.com>
2020-12-13 02:05:38 +1100

    new code commit

--- src/main/java/com/example/mvc_basics001/controller/HomeController.java
+++ src/main/java/com/example/mvc_basics001/controller/HomeController.java
@@ -16,19 +16,32 @@ public class HomeController {
 	@Autowired
 	private MessageService service;
 	
-	@GetMapping(value="/home")
+	@GetMapping(value="/home1")
     public String getGreetings(@ModelAttribute("newMessage")MessageForm newMessage, Model model) {
     	model.addAttribute("greetings", service.getMessageList());
     	return "home";
     }
 	
 	
-	@PostMapping(value="/home")
+	@PostMapping(value="/home1")
     public String postGreetings(@ModelAttribute("newMessage")MessageForm inputParam, Model model) {
 		service.getMessageList().add(inputParam.getText());
     	model.addAttribute("greetings", service.getMessageList());
     	inputParam.setText("");
     	return "home";
     }
+	
+	// Tricky one
+	/*
+	 * @GetMapping(value="/home") public String lowFive(MessageForm messageForm,
+	 * Model model) { service.getMessageList().add("low five.");
+	 * model.addAttribute("greetings", service.getMessageList()); return "home"; }
+	 * 
+	 * 
+	 * @PostMapping(value="/home") public String highFive(MessageForm messageForm,
+	 * Model model) { service.getMessageList().add("high five, " +
+	 * messageForm.getText() + "!"); model.addAttribute("greetings",
+	 * service.getMessageList()); return "home"; }
+	 */
 
 }
